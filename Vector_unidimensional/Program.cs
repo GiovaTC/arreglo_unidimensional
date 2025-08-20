9d204238058caf9292600c6df4f323170b6ca7de
gtapiero <giovanny.tapiero@pi.edu.co>
2025-08-20 13:33:27 -0500

    Agregar archivos de proyecto.

---
+++ Vector_unidimensional/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Crear un arreglo unidimensional de 5 elementos
+        int[] miVector = { 10, 20, 30, 40, 50 };
+
+        // Imprimir los elementos del arreglo
+        Console.WriteLine("Contenido del vector:");
+        for (int i = 0; i < miVector.Length; i++)
+        {
+            Console.WriteLine($"Elemento [{i}] = {miVector[i]}");
+        }
+
+        //Evitar que la consola se cierre inmediatamente
+        Console.WriteLine("\nPresiona una tecla para salir...");
+        Console.ReadKey();
+    }
+}
